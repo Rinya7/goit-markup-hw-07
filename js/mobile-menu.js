f86693baa91851f6b5c7fd4c389f91d367d356c8
Rinya7 <v.dmitriev0712@gmail.com>
2023-02-12 13:09:33 +0100

    no-scroll

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -3,6 +3,7 @@
     openMenuBtn: document.querySelector("[data-menu-open]"),
     closeMenuBtn: document.querySelector("[data-menu-close]"),
     menu: document.querySelector("[data-menu]"),
+    body: document.querySelector("body"),
   };
 
   refs.openMenuBtn.addEventListener("click", toggleMenu);
@@ -10,5 +11,10 @@
 
   function toggleMenu() {
     refs.menu.classList.toggle("is-hidden-mobil");
+    refs.body.classList.toggle("no-scroll");
+  }
+  function removeMenu() {
+    refs.menu.classList.add("is-hidden");
+    refs.body.classList.remove("no-scroll");
   }
 })();
